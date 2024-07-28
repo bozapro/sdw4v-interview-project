c15e67a9a1eedeb001a15fee79e309929de82bc5
Aleksandar Bozic <bozapro@gmail.com>
2024-07-28 23:05:13 +0200

    Initial project import

---
+++ src/index.js
@@ -0,0 +1,60 @@
+// quantity > 5
+// [{ product: 'Apple', totalPrice: 100 }]
+
+var arr = [
+  {
+    product: "Banana",
+    price: 100,
+    quantity: 50
+  },
+  {
+    product: "Apple",
+    price: 50,
+    quantity: 1
+  },
+  {
+    product: "Cherry",
+    price: 150,
+    quantity: 20
+  },
+  {
+    product: "Cherry",
+    price: 100,
+    quantity: 4
+  },
+  {
+    product: "Apple",
+    price: 50,
+    quantity: 2
+  },
+  {
+    product: "Cherry",
+    price: 100,
+    quantity: 50
+  },
+  {
+    product: "Banana",
+    price: 100,
+    quantity: 8
+  },
+  {
+    product: "Apple",
+    price: 10,
+    quantity: 20
+  },
+  {
+    product: "Cherry",
+    price: 100,
+    quantity: 30
+  },
+  {
+    product: "Apple",
+    price: 80,
+    quantity: 40
+  },
+  {
+    product: "Banana",
+    price: 190,
+    quantity: 2
+  }
+];
